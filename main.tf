63ca7c78b1179eb92e4744ffa152bd11049df564
FridayPush <jon@jonsimpson.net>
2020-01-08 14:50:18 -0800

    CSQL Backup Enabled

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Require google_sql_database_instance SSL
+# Require google_sql_database_instance backups
 resource "google_sql_database_instance" "postgres" {
   name             = "postgres-instance-946848957"
   database_version = "POSTGRES_11"
@@ -6,5 +6,8 @@ resource "google_sql_database_instance" "postgres" {
   settings {
     tier = "db-f1-micro"
 
+    backup_configuration {
+      enabled = true
+    }
   }
 }
