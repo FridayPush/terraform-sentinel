d477f34ff8160db3c7ba74754dda5843443aebf9
FridayPush <jon@jonsimpson.net>
2020-01-08 15:03:54 -0800

    CSQL authorized cidr 0000

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
-# Require google_sql_database_instance backups
+# Require google_sql_database_instance authorized network
+# CIDR 0.0.0.0/0
 resource "google_sql_database_instance" "postgres" {
   name             = "postgres-instance-946848957"
   database_version = "POSTGRES_11"
 
   settings {
     tier = "db-f1-micro"
-
-    backup_configuration {
-      enabled = true
+    ip_configuration {
+      authorized_networks {
+        value = "0.0.0.0/0"
+      }
     }
   }
 }
