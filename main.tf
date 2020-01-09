558c66547f8e2bbf80e4f50d4cdddeca1f083a80
FridayPush <jon@jonsimpson.net>
2020-01-09 15:10:21 -0800

    only provider

--- main.tf
+++ main.tf
@@ -3,30 +3,3 @@ provider "google" {
   project     = "faux-dev"
   region      = "us-central1"
 }
-
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id                  = "example_dataset"
-  friendly_name               = "test"
-  description                 = "This is a test description"
-  location                    = "US"
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = "default"
-  }
-
-  access {
-    role          = "OWNER"
-    user_by_email = "bob@bob.com"
-  }
-
-  access {
-    role          = "READER"
-    user_by_email = "bob@standardindustries.com"
-  }
-  access {
-    role          = "READER"
-    user_by_email = "bob@gaf.com"
-  }
-
-}
