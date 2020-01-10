81abe22f0397b26830e2da3b0c409aaa3b2414a1
FridayPush <jon@jonsimpson.net>
2020-01-09 16:06:49 -0800

    Resources no labels

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
 # Bigquery Prevent Domain Wide Access
-provider "google" {
-  project     = "faux-dev"
-  region      = "us-central1"
+resource "google_project" "my" {
+  name       = "Project with labels"
+  project_id = "custom-id-hex92319232"
+
 }
+
+resource "google_compute_disk" "default" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-8-jessie-v20170523"
+  physical_block_size_bytes = 4096
+}
+
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
+
+}
+
