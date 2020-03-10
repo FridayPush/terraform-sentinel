cfa708b661a358255651a713b3dda45b29e2d188
Jon Simpson <42477437+FridayPush@users.noreply.github.com>
2020-03-09 22:27:22 -0700

    sql kube pass

--- main.tf
+++ main.tf
@@ -95,94 +95,6 @@ resource "google_project" "project-w-labels" {
   }
 }
 
-resource "google_pubsub_topic" "topic-wo-labels" {
-  name = "example-topic"
-
-  labels = {
-    env = "test",
-  }
-}
-
-resource "google_bigquery_dataset" "dataset-wo-labels" {
-  dataset_id                  = "example_dataset"
-  friendly_name               = "test"
-  description                 = "This is a test description"
-  location                    = "US"
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    pipe = "world",
-    foo = "test"
-  }
-}
-
-resource "google_bigquery_table" "table-wo-label" {
-  dataset_id = "${google_bigquery_dataset.dataset-w-labels.dataset_id}"
-  table_id   = "bar"
-
-  labels = {
-    test = "default",
-    feature = "now"
-  }
-
-  schema = <<EOF
-[
-  {
-    "name": "permalink",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "The Permalink"
-  },
-  {
-    "name": "state",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "State where the head office is located"
-  }
-]
-EOF
-}
-
-resource "google_compute_image" "disk-wo-labels" {
-  name = "example-image"
-
-  raw_disk {
-    source = "https://storage.googleapis.com/bosh-cpi-artifacts/bosh-stemcell-3262.4-google-kvm-ubuntu-trusty-go_agent-raw.tar.gz"
-  }
-
-  labels = {
-    foo = "what"
-  }
-}
-
-resource "google_compute_instance" "instance-wo-labels" {
-  name         = "test"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-  
-  labels = {
-    env = "test",
-    fee = "what"
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral IP
-    }
-  }
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
-}
-
 resource "google_container_cluster" "mixed-label" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
@@ -208,6 +120,7 @@ resource "google_container_cluster" "mixed-label" {
     }
 
     labels = {
+      env = "test",
       foo = "bar"
     }
 
@@ -229,5 +142,9 @@ resource "google_sql_database_instance" "no-labels" {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
     tier = "db-f1-micro"
+    user_labels = {
+      env = "dev",
+      foo = "bar"
+    }
   }
 }
