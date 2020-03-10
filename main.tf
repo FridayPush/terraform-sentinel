4f7026638f606691f997e5a2d575da1680456633
Jon Simpson <42477437+FridayPush@users.noreply.github.com>
2020-03-09 23:46:28 -0700

    roles pass

--- main.tf
+++ main.tf
@@ -1,26 +1,57 @@
+resource "google_billing_account_iam_member" "ok-role" {
+  billing_account_id = "00AA00-000AAA-00AA0A"
+  role               = "roles/billingAdmin"
+  member             = "user:alice@gmail.com"
+}
+
+resource "google_kms_key_ring_iam_member" "ok-role" {
+  key_ring_id = "your-key-ring-id"
+  role        = "roles/encrypterDecrypter"
+  member      = "user:jane@example.com"
+}
+
+resource "google_organization_iam_member" "ok-role" {
+  org_id = "0123456789"
+  role   = "roles/folderAdmin"
+  member = "user:alice@gmail.com"
+}
+
+resource "google_storage_bucket_iam_binding" "ok-role" {
+  bucket = "my-bucket-id"
+  role = "roles/objectCreator"
+  members = [
+    "user:jane@example.com",
+  ]
+}
+
+resource "google_project_iam_binding" "ok-role" {
+  project = "your-project-id"
+  role    = "roles/anythingReally"
+
+  members = [
+    "user:jane@example.com",
+  ]
+}
+
+resource "google_service_account_iam_binding" "ok-role" {
+  service_account_id = "${google_service_account.sa.name}"
+  role               = "roles/saUser"
+
+  members = [
+    "user:jane@example.com",
+  ]
+}
+
+resource "google_service_account" "sa" {
+  account_id   = "my-service-account"
+  display_name = "A service account that only Jane can use"
+}
+
 /***************
 // Fail conditions
 *****************/
-data "google_iam_policy" "has-primitive" {
-  binding {
-    role = "Owner"
-    members = [
-      "serviceAccount:your-custom-sa@your-project.iam.gserviceaccount.com",
-    ]
-  }
+/*
 
-  binding {
-    role = "roles/storage.objectViewer"
-    members = [
-      "user:alice@gmail.com",
-    ]
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "editor" {
-  bucket = "gs://notreal"
-  policy_data = "${data.google_iam_policy.has-primitive.policy_data}"
-}
 
 resource "google_billing_account_iam_member" "primitive-role" {
   billing_account_id = "00AA00-000AAA-00AA0A"
@@ -66,7 +97,4 @@ resource "google_service_account_iam_binding" "primitive-role" {
   ]
 }
 
-resource "google_service_account" "sa" {
-  account_id   = "my-service-account"
-  display_name = "A service account that only Jane can use"
-}
+*/
