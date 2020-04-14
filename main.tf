ff95bd66e96330e5a4ab62f41223b53c7d4228fe
Jon Simpson <42477437+FridayPush@users.noreply.github.com>
2020-04-13 19:13:36 -0700

    Fail GKE repair scenario

--- main.tf
+++ main.tf
@@ -29,3 +29,42 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     ]
   }
 }
+
+
+resource "google_container_node_pool" "fail_pool_1" {
+  name       = "my-fail-pool"
+  location   = "us-central1"
+  cluster    = "${google_container_cluster.primary.name}"
+  node_count = 1
+  
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "fail_pool_2" {
+  name       = "my-failing-pool"
+  location   = "us-central1"
+  cluster    = "${google_container_cluster.primary.name}"
+  node_count = 1
+  
+  management {
+    auto_repair = true
+  }
+  
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
